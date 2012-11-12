abec0f64c1502e7bcd6bd0272b1a45d07edbb437
Eevee <git@veekun.com>
2012-11-11 21:37:30 -0800

    Quick restyling to make dialogue look a bit less like ass.

--- flora/engine/dialogue.py
+++ flora/engine/dialogue.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import cocos
+import pyglet
 
 class DialogueLayer(cocos.layer.Layer):
 
@@ -9,16 +10,25 @@ class DialogueLayer(cocos.layer.Layer):
     def __init__(self, dialogue_tree):
         super(DialogueLayer, self).__init__()
 
+        self.add( cocos.layer.ColorLayer(0, 0, 0, 64), z=0.1 )
+
         # Create the frame
-        border = cocos.layer.ColorLayer(64, 64, 64, 255, width=1152, height=272)
-        border.position = 64, 64
+        border = cocos.layer.ColorLayer(32, 32, 32, 255, width=1280 - 16*2, height=320 - 16*2)
+        border.position = 16, 16
         self.add(border, z=1)
 
-        bg = cocos.layer.ColorLayer(0, 0, 96, 255, width=1120, height=240)
-        bg.position = 80, 80
+        shadow = cocos.layer.ColorLayer(160, 160, 160, 255, width=1280 - 32*2, height=320 - 32*2)
+        shadow.position = 32, 32
+        self.add(shadow, z=1.9)
+
+        bg = cocos.layer.ColorLayer(208, 208, 208, 255, width=1280 - 32*2 - 4, height=320 - 32*2 - 4)
+        bg.position = 36, 32
         self.add(bg, z=2)
 
-        self.add(cocos.text.Label(dialogue_tree, font_name='Short Stack', font_size=40, height=80, width=960, anchor_y='top', position=(96, 304)), z=3)
+        self.add(cocos.text.Label(dialogue_tree, font_name='Short Stack', font_size=24, color=(0, 0, 0, 255), height=320 - 64*2, width=1280 - 64*2, anchor_y='top', position=(64, 320 - 64)), z=3)
+
+        # Top of the dialog box is at 304
+        self.add(cocos.sprite.Sprite(pyglet.resource.image('sprites/characters/maya/portrait-normal.png'), position=(64, 64), anchor=(0, 0)), z=0.5)
 
     def on_key_press(self, key, mod):
         # TODO self.kill() doesn't work with named layers  :|
